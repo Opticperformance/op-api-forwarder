d6c55f1724ea4a14b198e7138fb0262bf7e06e81
Quentin Ratinaud <quentin.ratinaud@gmail.com>
2024-01-18 22:37:57 +0100

    fix: export

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
 function createForwarder(baseUrl, axiosOptions = {}) {
     return async (req, res) => {
