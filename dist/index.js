7cd024bf433be3db2fb314ce7eb78e46f4a00c2e
Quentin Ratinaud <quentin.ratinaud@gmail.com>
2024-01-22 14:17:55 +0100

    fix: try catch + es2015

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,49 +15,37 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.createForwarder = void 0;
 const axios_1 = __importDefault(require("axios"));
 function createForwarder(baseUrl, axiosOptions = {}) {
-    return async (req, res) => {
+    return (req, res) => __awaiter(this, void 0, void 0, function* () {
         const bodyAvailable = 'body' in req;
-        const onEnd = async (requestData) => {
+        const onEnd = (requestData) => __awaiter(this, void 0, void 0, function* () {
             const { method, url, headers } = req;
             const body = bodyAvailable ? req.body : JSON.parse(requestData || '{}');
-            const mergedHeaders = {
-                ...axiosOptions.headers,
-                ...headers,
-            };
-            const mergedAxiosOptions = {
-                method: method,
-                url: `${baseUrl}${url}`,
-                headers: mergedHeaders,
-                data: body,
-                ...axiosOptions,
-            };
-            const axiosResponse = await (0, axios_1.default)(mergedAxiosOptions);
-            res.writeHead(axiosResponse.status, axiosResponse.statusText, {
-                'Content-Type': 'application/json',
-                ...axiosResponse.headers,
+            const mergedHeaders = Object.assign(Object.assign({}, axiosOptions.headers), headers);
+            const mergedAxiosOptions = Object.assign({ method: method, url: `${baseUrl}${url}`, headers: mergedHeaders, data: body }, axiosOptions);
+            try {
+                const axiosResponse = yield (0, axios_1.default)(mergedAxiosOptions);
+                res.writeHead(axiosResponse.status, axiosResponse.statusText, Object.assign({ 'Content-Type': 'application/json' }, axiosResponse.headers));
+                res.end(JSON.stringify(axiosResponse.data));
+            }
+            catch (error) {
+                console.error('Error forwarding request:', error.message);
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ success: false, error: 'Internal Server Error' }));
+            }
+        });
+        if (bodyAvailable) {
+            onEnd();
+        }
+        else {
+            let requestData = '';
+            req.setEncoding('utf8');
+            req.on('data', (chunk) => {
+                requestData += chunk;
             });
-            res.end(JSON.stringify(axiosResponse.data));
-        };
-        try {
-            if (bodyAvailable) {
-                onEnd();
-            }
-            else {
-                let requestData = '';
-                req.setEncoding('utf8');
-                req.on('data', (chunk) => {
-                    requestData += chunk;
-                });
-                req.on('end', () => {
-                    onEnd(requestData);
-                });
-            }
+            req.on('end', () => {
+                onEnd(requestData);
+            });
         }
-        catch (error) {
-            console.error('Error forwarding request:', error.message);
-            res.writeHead(500, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ success: false, error: 'Internal Server Error' }));
-        }
-    };
+    });
 }
 exports.createForwarder = createForwarder;
--- src/index.ts
+++ src/index.ts
@@ -23,35 +23,35 @@ function createForwarder(baseUrl: URL | RequestInfo, axiosOptions: AxiosRequestC
         ...axiosOptions, // Include any custom options provided by the user
       };
 
-      const axiosResponse = await axios(mergedAxiosOptions);
+      try {
+        const axiosResponse = await axios(mergedAxiosOptions);
 
-      res.writeHead(axiosResponse.status, axiosResponse.statusText, {
-        'Content-Type': 'application/json',
-        ...axiosResponse.headers,
-      } as OutgoingHttpHeaders);
-
-      res.end(JSON.stringify(axiosResponse.data));
+        res.writeHead(axiosResponse.status, axiosResponse.statusText, {
+          'Content-Type': 'application/json',
+          ...axiosResponse.headers,
+        } as OutgoingHttpHeaders);
+  
+        res.end(JSON.stringify(axiosResponse.data));
+      } catch (error: any) {
+        console.error('Error forwarding request:', error.message);
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ success: false, error: 'Internal Server Error' }));
+      }
     }
 
-    try {
-      if (bodyAvailable) {
-        onEnd();
-      } else {
-        let requestData = '';
-        req.setEncoding('utf8');
+    if (bodyAvailable) {
+      onEnd();
+    } else {
+      let requestData = '';
+      req.setEncoding('utf8');
 
-        req.on('data', (chunk) => {
-          requestData += chunk;
-        });
+      req.on('data', (chunk) => {
+        requestData += chunk;
+      });
 
-        req.on('end', () => {
-          onEnd(requestData);
-        });
-      }
-    } catch (error: any) {
-      console.error('Error forwarding request:', error.message);
-      res.writeHead(500, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ success: false, error: 'Internal Server Error' }));
+      req.on('end', () => {
+        onEnd(requestData);
+      });
     }
   };
 }
